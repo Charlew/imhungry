0522630cde52dcfa76ddad075d751ede9b505100
Filip Wicher <filip.wicher@allegro.pl>
2020-08-22 13:53:32 +0200

    hello world

---
+++ src/test/java/pl/codzisnaobiad/imhungry/ImhungryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.codzisnaobiad.imhungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImhungryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
