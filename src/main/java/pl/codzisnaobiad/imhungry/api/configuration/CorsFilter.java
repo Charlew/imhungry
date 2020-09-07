90578f2ffb8ff86c4e616d797ea2dbd8b0792e3b
Karol Wisniewski <karol.wisniewski@ecom.software>
2020-09-07 13:28:34 +0200

    Add cors filter

---
+++ src/main/java/pl/codzisnaobiad/imhungry/api/configuration/CorsFilter.java
@@ -0,0 +1,21 @@
+package pl.codzisnaobiad.imhungry.api.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class CorsFilter {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(@Value("${cors.allowed-origins}") String allowedOrigins) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowedOrigins.split(","));
+            }
+        };
+    }
+}
